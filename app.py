72177377476ed47bb66d5a60836eb2c11ae2abb6
Елизавета Плюснина <elizaplus@list.ru>
2024-09-17 12:50:08 +0700

    Исправление адресов

--- app.py
+++ app.py
@@ -6,20 +6,20 @@ def not_found(err):
     return "нет такой страницы", 404
 
 @app.route("/")
-@app.route("/web")
+@app.route("/lab1/web")
 def web():
     return """<!doctype html>
         <html> 
            <body> 
                <h1>web-сервер на flask</h1>
-               <a href="/author">author</a>
+               <a href="/ab1/author">author</a>
            </body>
         </html>""", 200, {
             "X-Server": "sample",
             "Content-Type": 'text/plain; charset=utf-8'
             }
 
-@app.route("/author")
+@app.route("/lab1/author")
 def author():
     name = "Плюснина Елизавета Евгеньевна" 
     group = "ФБИ-22"
@@ -31,7 +31,7 @@ def author():
                 <p>Студент: """ + name + """</p>
                 <p>Группа: """ + group + """</p>
                 <p>Факультет: """ + faculty + """</p>
-                <a href="/web">web</a>
+                <a href="/lab1/web">web</a>
            </body> 
         </html>"""
 
@@ -86,9 +86,9 @@ def reset_counter():
 '''
 
 
-@app.route("/info")
+@app.route("/lab1/info")
 def info():
-    return redirect("/author") # redirect - перенаправление
+    return redirect("/lab1/author") # redirect - перенаправление
 
 @app.route("/lab1/created")
 def created():
