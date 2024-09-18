b2901b65781b84e3556221b1eba7320e6b5d4740
Елизавета Плюснина <elizaplus@list.ru>
2024-09-18 18:07:45 +0700

    Добавление цветка

--- app.py
+++ app.py
@@ -295,7 +295,7 @@ def a():
 def a2():
     return 'со слэшем'
 
-flower_list = ('орхидея', 'гибискус', 'гипсофила', 'фиалка')
+flower_list = ['орхидея', 'гибискус', 'гипсофила', 'фиалка']
 
 @app.route('/lab2/flowers/<int:flower_id>')
 def flowers(flower_id):
@@ -303,3 +303,18 @@ def flowers(flower_id):
         return "такого цветка нет", 404
     else:
         return 'цветок: ' + flower_list[flower_id]
+
+@app.route('/lab2/add_flower/<name>')
+def add_flower(name):
+    flower_list.append(name)
+    return f'''
+<!doctype html>
+<html>
+    <body>
+    <h1>Добавлен новый цветок</h1>
+    <p>Название нового цветка: {name} </p>
+    <p>Всего цветов: {len(flower_list)} </p>
+    <p>Полный список: {flower_list} </p>
+    </body>
+</html>
+'''
