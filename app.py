ca504c91276c2f9e481ed60064c25a16f010ecae
Елизавета Плюснина <elizaplus@list.ru>
2024-09-18 17:20:59 +0700

    Слэши в адресе

--- app.py
+++ app.py
@@ -286,3 +286,11 @@ def delete():
         return 'Успешно: ресурс удалён', 200
     else:
         return 'Отказано: ресурс отсутствует', 400
+
+@app.route('/lab2/a')
+def a():
+    return 'без слэша'
+
+@app.route('/lab2/a/')
+def a2():
+    return 'со слэшем'
