ec88aa30eae07c446d80d6e301958517fb054973
Елизавета Плюснина <elizaplus@list.ru>
2024-09-19 16:23:59 +0700

    Имя студента

--- app.py
+++ app.py
@@ -321,4 +321,5 @@ def add_flower(name):
 
 @app.route('/lab2/example')
 def example():
-    return render_template('example.html')
+    name = 'Елиз Плюснина'
+    return render_template('example.html', name=name)
