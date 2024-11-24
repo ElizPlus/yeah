b4db102ec16ef6fcb889c5999c1451b89cdd1711
Елизавета Плюснина <elizaplus@list.ru>
2024-11-24 19:11:32 +0700

    Снятие аренды

--- lab6.py
+++ lab6.py
@@ -51,7 +51,47 @@ def api():
                     'id': id
                 }
             
+    if data['method'] == 'cancellation':
+        login = session.get('login')
+        if not login:
+            return {
+                'jsonrpc': '2.0',
+                'error': {
+                    'code': 1,
+                    'message': 'Unauthorized'
+                },
+                'id': id
+            }
+        
+        office_number = data['params']  
+        for office in offices:
+            if office['number'] == office_number:
+                if office['tenant'] == '':
+                    return {
+                        'jsonrpc': '2.0',
+                        'error': {
+                            'code': 3,
+                            'message': 'Office not booked'
+                        }, 
+                        'id': id
+                    }
+                if office['tenant'] != login:
+                    return {
+                        'jsonrpc': '2.0',
+                        'error': {
+                            'code': 4,
+                            'message': 'Cannot cancel someone else\'s booking'
+                        },
+                        'id': id
+                    }      
                 
+                office['tenant'] = ''
+                return {
+                    'jsonrpc': '2.0',
+                    'result': offices, 
+                    'id': id
+                }    
+    
 
     return {
         'jsonrpc': '2.0',
