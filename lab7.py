3048f36cedfc827dc1321b18cf22f5633eff93dd
Елизавета Плюснина <elizaplus@list.ru>
2024-12-07 21:41:44 +0700

    Добавление нового фильма по REST

--- lab7.py
+++ lab7.py
@@ -75,3 +75,11 @@ def put_film(id):
     film = request.get_json()
     films[id] = film
     return jsonify(films[id])
+
+
+@lab7.route('/lab7/rest-api/films/', methods=['POST'])
+def add_film():
+    film = request.get_json()
+    films.append(film)
+    new_index = len(films) - 1
+    return jsonify({"index": new_index}), 201
