5da6298d49f9b65cf348a4d1175a3537d1b57385
Елизавета Плюснина <elizaplus@list.ru>
2024-12-07 20:11:42 +0700

    Удаление выбранного фильма по REST

--- lab7.py
+++ lab7.py
@@ -58,3 +58,10 @@ def get_film(id):
         return jsonify({"error": "Фильм не найден"}), 404
     return jsonify(films[id])
 
+
+@lab7.route('/lab7/rest-api/films/<int:id>', methods=['DELETE'])
+def del_film(id):
+    if id < 0 or id >= len(films):
+        return jsonify({"error": "Фильм не найден"}), 404
+    del films[id]
+    return '', 204
