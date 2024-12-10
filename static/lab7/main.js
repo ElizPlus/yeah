403347d0594ba5a085ee40fb1f5f0fee3267fab9
Елизавета Плюснина <elizaplus@list.ru>
2024-12-10 23:01:24 +0700

    Добавление фильма

--- static/lab7/main.js
+++ static/lab7/main.js
@@ -27,34 +27,6 @@ function fillFilmList() {
                 deleteFilm(i, films[i].title_ru);
             };
 
-            function fillFilmList() {
-
-            }
-
-            function deleteFilm(id, title) {
-                if(! confirm(`Вы точно хотите удалить фильм "${title}"?:)`))
-                fetch(`/lab7/rest-api/films/${id}`, {method: 'DELETE'})
-                    .then(function () {
-                        fillFilmList();
-                    });
-            }
-
-            function showModal() {
-                document.querySelector('div.modal').style.display = 'block';
-            }
-
-            function hideModal() {
-                document.querySelector('div.modal').style.display = 'none';
-            }
-
-            function cancel() {
-                hideModal();
-            }
-
-            function addFilm() {
-                showModal();
-            }
-
             tdActions.append(editButton);
             tdActions.append(delButton);
 
@@ -67,4 +39,57 @@ function fillFilmList() {
         }
         
     })
+}
+
+function deleteFilm(id, title) {
+    if(! confirm(`Вы точно хотите удалить фильм "${title}"?:)`))
+        return;
+    
+    fetch(`/lab7/rest-api/films/${id}`, {method: 'DELETE'})
+        .then(function () {
+            fillFilmList();
+        });
+}
+
+function showModal() {
+    document.querySelector('div.modal').style.display = 'block';
+}
+
+function hideModal() {
+    document.querySelector('div.modal').style.display = 'none';
+}
+
+function cancel() {
+    hideModal();
+}
+
+function addFilm() {
+    document.getElementById('id').value = '';
+    document.getElementById('title-ru').value = '';
+    document.getElementById('title').value = '';
+    document.getElementById('year').value = '';
+    document.getElementById('description').value = '';
+    showModal();
+}
+
+function sendFilm() {
+    const film = {
+        title: document.getElementById('title').value,
+        title_ru: document.getElementById('title-ru').value,
+        year: document.getElementById('year').value,
+        description: document.getElementById('description').value
+    }
+
+    const url = `/lab7/rest-api/films/`;
+    const method = 'POST';
+
+    fetch(url, {
+        method: method,
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(film)
+    })
+    .then(function () {
+        fillFilmList();
+        hideModal();
+    });
 }
